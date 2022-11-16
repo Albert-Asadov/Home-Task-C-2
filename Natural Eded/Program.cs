c8d4e5857c0a4f8efa93eb4078496dfe590c308f
Albert-Asadov <alberma@code.edu.az>
2022-11-16 10:53:15 +0400

    Natural Eded

---
+++ Natural Eded/Program.cs
@@ -0,0 +1,16 @@
+﻿int naturalEded = 581;
+int count = 0;
+
+for (int i = 1; i < naturalEded; i *= 10)
+{
+    count = i;
+
+}
+
+if (count > 0)
+{
+    count *= 10;
+
+    int number = 3 * count + naturalEded;
+    Console.WriteLine(number);
+}
